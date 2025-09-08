b85326d78c7cdb13f7bdcb4f4f42a9abea18eca0
Naveen-pntk32 <pnavaneethakrishnan07@gmail.com>
2025-09-08 23:26:28 +0530

    favicon Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,16 @@ import CustomCursor from "../components/customcursor";
 export const metadata: Metadata = {
   title: "Naveen-PNTk",
   description: "Personal portfolio website of Navaneethakrishnan",
+  manifest: "/favicon/site.webmanifest",
+  themeColor: "#ffffff",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon/favicon.ico" },
+    ],
+    apple: "/favicon/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
